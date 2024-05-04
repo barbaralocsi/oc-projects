a97238f97e75a0e082a94d1d03f608d660663050
Barbara Locsi <locsi.barbara@gmail.com>
2024-05-04 18:26:39 +0200

    Handle user not logged in

--- oc-project.ps1
+++ oc-project.ps1
@@ -1,1 +1,12 @@
-oc project $(oc projects -q | fzf --height 40%)
+try {
+    $projects = oc projects -q
+    if ($projects) {
+        oc project $($projects | fzf --height 40%)
+    }
+    else {
+        Write-Host "No projects available. Are you logged in?"
+    }
+}
+catch {
+    Write-Host "An error occurred: $_"
+}
